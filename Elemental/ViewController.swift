5699be34629612c041e2a48fc945a294ac44f599
Gaetano Herman <hermangaetano@gmail.com>
2016-01-14 22:41:44 +0100

    Initial Commit

---
+++ Elemental/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Elemental
+//
+//  Created by Gaetano Herman on 14/01/16.
+//  Copyright © 2016 Gaetano Herman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
